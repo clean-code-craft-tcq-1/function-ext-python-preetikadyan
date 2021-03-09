16ff3d6a23e3cd00cfc613040a4ee4776fe55443
PREETI <preetikadyan634@gmail.com>
2021-03-09 22:33:33 +0530

    Update report_breach_level_EN_warning.py

--- report_breach_level_EN_warning.py
+++ report_breach_level_EN_warning.py
@@ -2,9 +2,9 @@
 
 def report_breach_level_EN_warning(parameters_limit,parameter_name,parameter_value,parameter_min,parameter_max,lang): 
     tolerance = parameter_max * 0.05
-    if parameter_value >= parameter_min and parameter_value <= parameter_min + tolerance :
-        parameters_limit[parameter_name] = 'LOW_WARNING'
-    elif parameter_value <= parameter_max and parameter_value >= parameter_max - tolerance :
+    for parameter_value in range(parameter_min,parameter_min + tolerance + 1.00):
+        parameters_limit[parameter_name] = '_LOW_WARNING'
+    #elif parameter_value <= parameter_max and parameter_value >= parameter_max - tolerance :
+    for parameter_value in range(parameter_min,parameter_max - tolerance + 1.00):
         parameters_limit[parameter_name] = 'HIGH_WARNING' 
-    else:
-        parameters_limit[parameter_name] = 'NORMAL'
+    parameters_limit[parameter_name] = 'NORMAL'
