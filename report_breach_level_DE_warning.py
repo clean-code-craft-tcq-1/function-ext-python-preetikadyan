02177afa72ec64c5f4ddb1bd162e50a907029fe8
PREETI <preetikadyan634@gmail.com>
2021-03-09 22:27:52 +0530

    Update report_breach_level_DE_warning.py

--- report_breach_level_DE_warning.py
+++ report_breach_level_DE_warning.py
@@ -2,8 +2,8 @@
 
 def report_breach_level_DE_warning(parameters_limit,parameter_name,parameter_value,parameter_min,parameter_max,lang): 
     tolerance = parameter_max * 0.05
-    min_tolerance = parameter_min + tolerance + 1
-    max_tolerance = parameter_max - tolerance + 1
+    min_tolerance = parameter_min + tolerance + 1.00
+    max_tolerance = parameter_max - tolerance + 1.00
     #if parameter_value >= parameter_min and parameter_value <= parameter_min + tolerance :
     for parameter_value in range(parameter_min,min_tolerance):
         parameters_limit[parameter_name] = 'NIEDRIGE WARNUNG'
