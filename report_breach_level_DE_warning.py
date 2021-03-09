0bedcb5ab1dd07c74aa6bda204deb3aba2f2298b
PREETI <preetikadyan634@gmail.com>
2021-03-09 22:16:49 +0530

    Update report_breach_level_DE_warning.py

--- report_breach_level_DE_warning.py
+++ report_breach_level_DE_warning.py
@@ -2,9 +2,10 @@
 
 def report_breach_level_DE_warning(parameters_limit,parameter_name,parameter_value,parameter_min,parameter_max,lang): 
     tolerance = parameter_max * 0.05
-    if parameter_value >= parameter_min and parameter_value <= parameter_min + tolerance :
+    #if parameter_value >= parameter_min and parameter_value <= parameter_min + tolerance :
+    for parameter_value in range(parameter_min,parameter_min + tolerance + 1)
         parameters_limit[parameter_name] = 'NIEDRIGE WARNUNG'
-    elif parameter_value <= parameter_max and parameter_value >= parameter_max - tolerance :
+    #elif parameter_value <= parameter_max and parameter_value >= parameter_max - tolerance :
+    for parameter_value in range(parameter_min,parameter_max - tolerance + 1)
         parameters_limit[parameter_name] = 'HOHE WARNUNG' 
-    else:
-        parameters_limit[parameter_name] = 'NORMAL'
+    parameters_limit[parameter_name] = 'NORMAL'
