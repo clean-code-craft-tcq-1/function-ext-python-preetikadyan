8a4ee92d5092b518ee3144bf06bc718407e70ee5
PREETI <preetikadyan634@gmail.com>
2021-03-09 21:51:14 +0530

    Update collect_battery_parameters.py

--- collect_battery_parameters.py
+++ collect_battery_parameters.py
@@ -1,7 +1,8 @@
-
+from collect_out_of_range_parameters_DE import *
+from collect_out_of_range_parameters_EN import *
 
 def collect_battery_parameters(parameters_alert,parameter_name,parameter_value,parameter_limit,lang):
    if lang == 'DE':
-     collect_out_of_range_parameters_DE(parameters_alert,parameter_name,parameter_value,parameter_limit['min'],parameter_limit['max'],'DE')
+     collect_out_of_range_parameters_DE(parameters_alert,parameter_name,parameter_value,parameter_limit['min'],parameter_limit['max'],lang)
    else:
      collect_out_of_range_parameters_EN(parameters_alert,parameter_name,parameter_value,parameter_limit['min'],parameter_limit['max'],lang)
