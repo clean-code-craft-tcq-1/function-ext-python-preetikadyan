8a92e2290db5b783e8360184e32c4a932da48c9e
PREETI <preetikadyan634@gmail.com>
2021-03-15 13:40:56 +0530

    Update report_battery_parameters.py

--- report_battery_parameters.py
+++ report_battery_parameters.py
@@ -3,6 +3,7 @@ from check_limits import *
 
 def report_battery_parameters(Battery_Life_Parameters,lang):
      parameters_alert_dict = {}
+     parameters_action_dict = {}
      for battery_parameter in Battery_Life_Parameters:
-          collect_battery_parameters(parameters_alert_dict,battery_parameter,Battery_Life_Parameters[battery_parameter],limit[battery_parameter],lang)
-     return parameters_alert_dict
+          collect_battery_parameters(parameters_action_dict,parameters_alert_dict,battery_parameter,Battery_Life_Parameters[battery_parameter],limit[battery_parameter],lang)
+     return parameters_alert_dict,parameters_action_dict
