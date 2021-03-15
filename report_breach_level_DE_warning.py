5f3bb62b680e6925cd87b3154cf3a90ce8ea9dc1
PREETI <preetikadyan634@gmail.com>
2021-03-15 13:46:28 +0530

    Update report_breach_level_DE_warning.py

--- report_breach_level_DE_warning.py
+++ report_breach_level_DE_warning.py
@@ -1,11 +1,14 @@
 
 
-def report_breach_level_DE_warning(parameters_limit,parameter_name,parameter_value,parameter_min,parameter_max,lang): 
+def report_breach_level_DE_warning(parameters_action,parameters_limit,parameter_name,parameter_value,parameter_min,parameter_max,lang): 
     tolerance = parameter_max * 0.05
     #if parameter_value >= parameter_min and parameter_value <= parameter_min + tolerance :
     for parameter_value in range(parameter_min,int(parameter_min + tolerance + 1.00)):
         parameters_limit[parameter_name] = 'NIEDRIGE WARNUNG'
+        parameters_action[parameter_name] = 'Erhöhen Sie den angegebenen Parameter, um eine Verletzung der unteren Grenze zu vermeiden'
     #elif parameter_value <= parameter_max and parameter_value >= parameter_max - tolerance :
     for parameter_value in range(parameter_min,int(parameter_max - tolerance + 1.00)):
         parameters_limit[parameter_name] = 'HOHE WARNUNG' 
+        parameters_action[parameter_name] = 'Schalten Sie den Lüfter ein, um eine Überschreitung der Obergrenze zu vermeiden'
     parameters_limit[parameter_name] = 'NORMAL'
+    parameters_action[parameter_name] = 'Alles ist in Ordnung, nichts muss getan werden'
