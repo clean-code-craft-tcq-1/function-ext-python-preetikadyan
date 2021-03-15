91960c9d7604a933b28f61afe601f4d4189f7f5c
PREETI <preetikadyan634@gmail.com>
2021-03-15 13:44:48 +0530

    Update collect_out_of_range_parameters_EN.py

--- collect_out_of_range_parameters_EN.py
+++ collect_out_of_range_parameters_EN.py
@@ -1,9 +1,9 @@
-from report_breach_level_EN_warning import *
-
-def collect_out_of_range_parameters_EN(parameters_exceeded_limit,parameter_name,parameter_value,parameter_min,parameter_max,lang):
+def collect_out_of_range_parameters_EN(parameters_action,parameters_exceeded_limit,parameter_name,parameter_value,parameter_min,parameter_max,lang):
     if parameter_value < parameter_min:
          parameters_exceeded_limit[parameter_name] = 'LOW_BREACH'
+         parameters_action[parameter_name] = 'Parameter needs to be increased for the given parameter to cross lower limit'
     elif parameter_value > parameter_max:
          parameters_exceeded_limit[parameter_name] = 'HIGH_BREACH'
+         parameters_action[parameter_name] = 'Switch on fan to bring the given parameter below max limit'
     else:    
-        report_breach_level_EN_warning(parameters_exceeded_limit,parameter_name,parameter_value,parameter_min,parameter_max,lang)
+        report_breach_level_EN_warning(parameters_action,parameters_exceeded_limit,parameter_name,parameter_value,parameter_min,parameter_max,lang)
