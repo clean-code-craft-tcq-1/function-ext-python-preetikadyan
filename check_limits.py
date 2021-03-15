d144308cbe94dea7467fd4902b9abc13b3776915
PREETI <preetikadyan634@gmail.com>
2021-03-15 13:38:48 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -7,5 +7,5 @@ limit = {
         }           
     
 if __name__ == '__main__':
-     raise_alert_for_battery_parameters({'temperature' : 25, 'state_of_charge' : 70, 'charge_rate' : 0.7},'EN')
-     raise_alert_for_battery_parameters({'temperature' : 50, 'state_of_charge' : 85, 'charge_rate' : 0},'DE')
+     raise_alert_for_battery_parameters({'temperature' : 25, 'state_of_charge' : 70, 'charge_rate' : 0.7},'DE')
+     raise_alert_for_battery_parameters({'temperature' : 50, 'state_of_charge' : 85, 'charge_rate' : 0},'EN')
